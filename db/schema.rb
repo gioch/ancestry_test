61c99eb7fa1d0a25a69ea92733eaa6e307052406
George Chkhvirkia <georgechkhvirkia@gmail.com>
2017-10-17 16:35:15 +0400

    added ancestry and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017113407) do
+ActiveRecord::Schema.define(version: 20171017114705) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "products", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+### Categories
+
+women = Category.create(name: 'Women')
+women_bags = Category.create(name: 'Bags', parent: women)
+women_clothes = Category.create(name: 'Clothes', parent: women)
+women_shirts = Category.create(name: 'Shirts', parent: women_clothes)
+women_jeans = Category.create(name: 'Jeans', parent: women_clothes)
+skinny_jeans = Category.create(name: 'Skinny', parent: women_jeans)
+straight_jeans = Category.create(name: 'Straight', parent: women_jeans)
+
+men = Category.create(name: 'Men')
+men_accessories = Category.create(name: 'Accessories', parent: men)
+men_shoes = Category.create(name: 'Shoes', parent: men)
+
+
+### Products
+Product.create(name: 'Some Woman Product', category: women)
+Product.create(name: 'Woman Clothes', category: women_clothes)
+Product.create(name: 'Skinny Jeans', category: skinny_jeans)
+Product.create(name: 'Straight Jeans', category: straight_jeans)
+
+Product.create(name: 'Man Product', category: men)
+Product.create(name: 'Man Accessory', category: men_accessories)
+Product.create(name: 'Man Shoe', category: men_shoes)
